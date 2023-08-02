fe84b8650832dda2a365cd9dd77183ba6662d0e9
sourav-kundu <66624530+sourav-kundu@users.noreply.github.com>
2023-08-02 13:31:06 +0530

    changed logo class element for tests to fail

--- pageobjects/bstack-demo/home.page.js
+++ pageobjects/bstack-demo/home.page.js
@@ -9,7 +9,9 @@ class HomePage extends Page {
      */
 
     get browserstackLogo() {
-        return $('.Navbar_logo__26S5Y');
+        // return $('.Navbar_logo__26S5Y');
+        return $('.Navbar_logo__26Y');
+
     }
 
     get signInBtn() {
