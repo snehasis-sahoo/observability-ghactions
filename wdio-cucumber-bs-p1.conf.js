287bd60edea31d443d6871a5deed50dbccae24b3
Sourav Kundu <66624530+sourav-kundu@users.noreply.github.com>
2023-09-18 20:07:12 +0530

    added env variable printing in conf file

--- wdio-cucumber-bs-p1.conf.js
+++ wdio-cucumber-bs-p1.conf.js
@@ -323,8 +323,9 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+     onComplete: function(exitCode, config, capabilities, results) {
+        console.log(process.env.BS_TESTOPS_BUILD_HASHED_ID);
+     },
     /**
     * Gets executed when a refresh happens.
     * @param {String} oldSessionId session ID of the old session
